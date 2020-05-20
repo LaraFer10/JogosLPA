57a3119f7c75c67224dba99ca1ad1e6d358f467c
LaraFer10 <lara.ferrer001@gmail.com>
2020-05-20 17:43:15 -0300

    Menu com usuario identificado

--- JogoLpa/src/br/ucsal/Jogo.java
+++ JogoLpa/src/br/ucsal/Jogo.java
@@ -8,9 +8,12 @@ public class Jogo {
 
 	public static void main(String[] args) {
 		int opt;
+		String[] vetUsuarios = new String[2]; 
 		
+		//Identifica os usuarios retornando um vetor de nomes
+		vetUsuarios = identificaUsuario();
 		// Recebe a opção escolhida pelo usuario
-		opt = escolhaMenu();
+		opt = escolhaMenu(vetUsuarios);
 		
 		// Leva o usuário para a execussão do jogo escolhido
 		switch(opt) {
@@ -32,13 +35,43 @@ public class Jogo {
 		}
 	}
 	
+	//Identifica os Usuarios que vao jogar e retorna um vetor com nomes para o main
+	public static String[] identificaUsuario() {
+		String[] usuarios = new String[2];
+		String respo;
+		Scanner input = new Scanner(System.in);
+		tracoHorizontal();
+		tracoVertical();
+		System.out.print("------ Bem-Vindo ao Jogos Divertidos! ------");
+		tracoVertical();
+		enter();
+		tracoHorizontal();
+		System.out.println("Identifique-se com seu Nome:");
+		usuarios[0] = input.next();
+		System.out.println("Existe outro jogador? (sim | nao)");
+		respo = input.next();
+		if(respo == "sim") {
+			System.out.println("Identifique o outro jogador com o Nome:");
+			usuarios[1] = input.next();
+		}else {
+			return usuarios;
+		}
+		return usuarios;
+	}
+	
 	/*Printa na tela o menu junto com as opções escolhidas pelo usuario
 	* e retorna o numero digitado para a classe principal
 	*/
-	public static int escolhaMenu() {
+	public static int escolhaMenu(String[] usuarios) {
 		Scanner input = new Scanner(System.in);
 		int opcao;
-		System.out.println("##--- Bem-Vindo ao Escolhas divertidas! ---##");
+		if(usuarios[1]!= null) {
+			
+			System.out.println("## Bem-Vindos(as) "+usuarios[0]+" e "+usuarios[1]+" ##");
+		}else {
+			System.out.println("## Bem-Vindo(a) "+usuarios[0]+" ##");
+		}
+		System.out.println("##-------- Este é o menu de jogos! --------##");
 		tracoHorizontal();
 		tracoVertical();
 		System.out.print("(1) Jogo da Forca                           ");
@@ -75,3 +108,4 @@ public class Jogo {
 	}
 
 }
+
